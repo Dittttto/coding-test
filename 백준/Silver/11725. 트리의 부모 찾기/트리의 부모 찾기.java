b3ae0ac015c60822b8137a5b893be763a6513f0e
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-03-27 12:03:26 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1404 ms, Memory: 65832 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer srt;
+        int T = Integer.parseInt(reader.readLine());
+        ArrayList<LinkedList<Integer>> map = new ArrayList<>();
+        for (int i = 0; i <= T; i++) {
+            map.add(i, new LinkedList<>());
+        }
+
+        boolean[] visited = new boolean[T+1];
+        int[] parent = new int[T+1];
+        for (int i = 0; i < T - 1; i++) {
+            srt = new StringTokenizer(reader.readLine());
+            int A = Integer.parseInt(srt.nextToken());
+            int B = Integer.parseInt(srt.nextToken());
+            map.get(A).offerLast(B);
+            map.get(B).offerLast(A);
+        }
+        dfs(map, visited, parent);
+        for (int i = 2; i < parent.length; i++) {
+            System.out.println(parent[i]);
+        }
+    }
+
+    public static void dfs(ArrayList<LinkedList<Integer>> map, boolean[] visited, int[] parent){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+
+        while(!stack.isEmpty()) {
+            int cur = stack.pop();
+            while(!map.get(cur).isEmpty()) {
+                LinkedList<Integer> curArr = map.get(cur);
+                if(curArr.size() > 0) {
+                    int num = curArr.pollFirst();
+                    if(!visited[num]) {
+                        stack.push(num);
+                        visited[num] = true;
+                        parent[num] = cur;
+                    }
+                }
+            }
+        }
+    }
+}
