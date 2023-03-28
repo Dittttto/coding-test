b372e559d32a9e9c692f45a80a2b29ff65cfdde2
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-03-28 12:27:33 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 2052 ms, Memory: 109000 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] visited;
+    static int depth = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer srt = new StringTokenizer(reader.readLine());
+        int N = Integer.parseInt(srt.nextToken());
+        int M = Integer.parseInt(srt.nextToken());
+        int R = Integer.parseInt(srt.nextToken());
+
+        visited = new int[N+1];
+        ArrayList<LinkedList<Integer>> map = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            map.add(i, new LinkedList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            srt = new StringTokenizer(reader.readLine());
+            int A = Integer.parseInt(srt.nextToken());
+            int B = Integer.parseInt(srt.nextToken());
+
+            map.get(A).offer(B);
+            map.get(B).offer(A);
+        }
+
+        sort(map);
+        bfs(map, R);
+        for (int i = 1; i < visited.length ; i++) {
+            System.out.println(visited[i]);
+        }
+    }
+
+    public static void bfs(ArrayList<LinkedList<Integer>> map, int R){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(R);
+        visited[R] = depth;
+
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            LinkedList<Integer> next = map.get(cur);
+            while(!next.isEmpty()) {
+                int nextV = next.pollFirst();
+                if(visited[nextV] == 0) {
+                    queue.offer(nextV);
+                    depth += 1;
+                    visited[nextV] = depth;
+                }
+            }
+
+        }
+    }
+
+    public static void sort(ArrayList<LinkedList<Integer>> map) {
+        for(LinkedList<Integer> row: map) {
+            row.sort(Comparator.naturalOrder());
+        }
+    }
+}
