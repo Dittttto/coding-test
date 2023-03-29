a3c83a0db897de2a4c101db7ac4f550a15a446af
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-03-29 23:25:07 +0900

    [Silver III] Title: 모든 순열, Time: 340 ms, Memory: 33944 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -1,46 +1,41 @@
 import java.io.*;
 
 public class Main {
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(reader.readLine());
         int[] arr = new int[N];
+        boolean[] visited = new boolean[N+1];
+        int[] out = new int[N];
         for (int i =1; i <= N; i++) {
             arr[i-1] = i;
         }
 
-        do {
-            for(int num: arr) {
-                writer.write(num + " ");
-            }
-            writer.write("\n");
-        }while (nextPermutation(arr, N));
+        permutation(arr, visited, out, 0, N, N);
 
         writer.flush();
         writer.close();
     }
 
-    public static boolean nextPermutation(int[] arr, int N) {
-        int i = N -1;
-        while(i > 0 && arr[i-1] > arr[i]) i-=1;
-        if(i == 0) return false;
-        int j = N-1;
-        while(arr[i-1] >= arr[j]) j-=1;
-        swap(arr, i-1, j);
-        j = N -1;
-        while(i < j) {
-            swap(arr, i, j);
-            i+=1;
-            j -=1;
+     public static void permutation(int[] arr, boolean[] visited, int[] out, int depth, int N, int M) throws IOException {
+        if(depth == M) {
+            for(int num: out) {
+                writer.write(num + " ");
+            }
+            writer.write("\n");
+            return;
         }
 
-        return true;
-    }
-
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        for (int i = 0; i < N; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                out[depth] = arr[i];
+                permutation(arr, visited, out, depth +1, N, M);
+                visited[i] = false;
+                out[depth] = 0;
+            }
+        }
     }
 }
