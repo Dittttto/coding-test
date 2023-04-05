017fb4517231c3a6f7ab646db955e916068269c4
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-04-05 16:50:29 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 10.72 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,13 +1,10 @@
 import java.util.*;
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        HashMap<String, Integer> hm = new HashMap<>(){{
-            put("R", 0);put("T", 1);put("C", 2);put("F", 3);
-            put("J", 4);put("M", 5);put("A", 6);put("N", 7);
-        }};
-        String[] name = new String[]{"R","T","C","F","J","M","A","N"};
+        ArrayList<String> nameArr = new ArrayList<>(Arrays.asList("R","T","C","F","J","M","A","N"));
         int[][] index = new int[][]{{0,0}, {0,0}, {0,0}, {0,0}};
         int[] score = new int[]{3,2,1,0,1,2,3};
+        String answer ="";
         
         for(int i = 0; i < survey.length; i++){
             String[] names = survey[i].split("");
@@ -18,24 +15,20 @@ class Solution {
             int idx = 0;
             if(choice == 4)continue;
             if(choice < 4) {
-                idx = hm.get(A);
+                idx = nameArr.indexOf(A);
             }else {
-                idx = hm.get(B);
+                idx = nameArr.indexOf(B);
             }
             
             index[idx/n][idx%n] += score[choice-1];
-            
         }
-        String answer ="";
         
         for(int i = 0; i< index.length; i++) {
             if(index[i][0] >= index[i][1]){  
-                answer += name[i * 2];
+                answer += nameArr.get(i * 2);
             } else{ 
-                answer += name[i * 2+1];
+                answer += nameArr.get(i * 2+1);
             }
-            
-            
         }
         return answer;
     }
