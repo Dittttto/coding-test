38a3905d0c8d980184dbfed566778990f72ccb81
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-04-07 22:18:05 +0900

    [level 1] Title: 신규 아이디 추천, Time: 4.68 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,6 +1,24 @@
 class Solution {
-    public String solution(String new_id) {
+    public String solution2(String new_id) {
         return run(new_id);
+    }
+    
+    public String solution(String new_id) {
+        new_id = new_id.toLowerCase();
+        new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
+        new_id = new_id.replaceAll("\\.+", ".");
+        new_id = new_id.replaceAll("^\\.+|\\.+$", "");
+        if(new_id.length() == 0) new_id = "a";
+        if(new_id.length() >= 16){
+            new_id = new_id.substring(0, 15).replaceAll("\\.+$", "");
+        }
+        if(new_id.length() <= 2) {
+            while(new_id.length() <3) {
+                new_id += new_id.charAt(new_id.length() -1 );
+            }
+        }
+        
+        return new_id;
     }
     public static String run(String new_id) {
         String answer = "";
