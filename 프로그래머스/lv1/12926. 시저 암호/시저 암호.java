eb30c424d70315eee566a1921780a06ed84f73b0
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-04-07 17:49:48 +0900

    [level 1] Title: 시저 암호, Time: 1.08 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String s, int n) {
+        char[] arr = s.toCharArray();
+        for(int i = 0; i < arr.length; i++) {
+            char tmp = arr[i];
+            if(tmp ==' ') continue;
+            for(int j= 0; j < n;j++) {
+                tmp+=1;
+                if(tmp >= 'a') {
+                tmp  = (char)((tmp - 'a') %(int)('z' - 'a'+1) + 'a');
+                }else {
+                    tmp  = (char)((tmp - 'A') %(int)('Z' - 'A'+1) + 'A');    
+                }
+            }
+            arr[i] = tmp;
+        }
+        String answer = new String(arr);
+        return answer;
+    }
+}
