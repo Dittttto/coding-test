39e4debef12c86d10f3cd7ceb51bc6c7933135ff
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-05-18 18:12:53 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -11,7 +11,7 @@ public class Main {
         int r = 31;
         long sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            sum += ((arr[i] - 'a' + 1) * Math.pow(r, i)) % m;
+            sum += ((arr[i] - 'a' + 1) * Math.pow(r, i));
         }
         System.out.println(sum % m);
     }
