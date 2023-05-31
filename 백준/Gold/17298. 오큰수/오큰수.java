69a95bc1c39934569d02400ca42c3d7d5f4085e6
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-05-31 23:25:54 +0900

    [Gold IV] Title: 오큰수, Time: 1056 ms, Memory: 158364 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,51 +1,51 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer stringTokenizer;
         int T = Integer.parseInt(reader.readLine());
-        Stack<Integer> stack = new Stack<>();
+        Stack<Node> stack = new Stack<>();
         int[] numbers = new int[T];
         int[] answer = new int[T];
-        int depth = 1;
 
         stringTokenizer = new StringTokenizer(reader.readLine());
         for (int i = 0; i < T; i++) {
             numbers[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
-        stack.push(0);
-        answer[T-1] = -1;
 
-        while(depth < T) {
-            int num = numbers[depth];
-            int top = stack.peek();
+        for (int i = 0; i < T; i++) {
+            int num = numbers[i];
 
-            if(numbers[top] < num) {
-                answer[top] = num;
+            while(!stack.isEmpty() && stack.peek().val < num) {
+                answer[stack.peek().idx] = num;
                 stack.pop();
-                if(stack.isEmpty()){
-                    stack.push(depth);
-                    depth +=1;
-                }
-            }else {
-                stack.push(depth);
-                depth +=1;
             }
+
+            stack.push(new Node(i, num));
         }
 
         while(!stack.isEmpty()){
-            answer[stack.pop()] = -1;
+            Node pop = stack.pop();
+            answer[pop.idx] = -1;
         }
 
-        for(int i: answer) {
+        for (int i : answer) {
             writer.write(i + " ");
         }
-
         writer.flush();
         writer.close();
+    }
+
+    static class Node{
+        int idx = 0;
+        int val = 0;
+
+        public Node(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
+        }
     }
 }
