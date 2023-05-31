e11f9e3471c6e5ff81d6b638176877a7653040fe
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-05-31 23:08:34 +0900

    [Silver II] Title: 스택 수열, Time: 2588 ms, Memory: 56032 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,56 +1,45 @@
+import java.util.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer srt = new StringTokenizer(reader.readLine());
-        int T = Integer.parseInt(srt.nextToken());
+        int N = Integer.parseInt(reader.readLine());
+        Queue<Integer> q = new LinkedList<>();
         Stack<Integer> stack = new Stack<>();
-        int[] numbers = new int[T];
-        int[] arr = new int[T];
-
-        for (int i = 0; i < T; i++) {
-            srt = new StringTokenizer(reader.readLine());
-            arr[i] = Integer.parseInt(srt.nextToken());
-            numbers[i] = i + 1;
-        }
-
-        int depth = 0;
-        int idx = 0;
-        boolean no = false;
         ArrayList<String> result = new ArrayList<>();
-        while (depth < T) {
-            if (stack.empty()) {
-                stack.push(numbers[idx]);
-                result.add("+");
-                idx += 1;
+        for (int i = 0; i < N; i++) {
+            q.offer(Integer.parseInt(reader.readLine()));
+        }
+
+        int num = q.poll();
+
+        for (int i = 1; i <= N; i++) {
+
+            if(!stack.isEmpty() && stack.peek() > i) {
+                break;
             }
 
-            if (stack.peek() == arr[depth]) {
-                result.add("-");
-                stack.pop();
-                depth += 1;
-            } else {
-                if (idx >= T) {
-                    no = true;
-                    break;
-                }
+            stack.push(i);
+            result.add("+");
 
-                stack.push(numbers[idx]);
-                result.add("+");
-                idx += 1;
+            while(!stack.isEmpty() && stack.peek() == num) {
+                stack.pop();
+                result.add("-");
+
+                if(q.isEmpty()) break;
+                num = q.poll();
             }
         }
-        if (no) {
+
+        if(!stack.isEmpty()) {
             System.out.println("NO");
             return;
         }
 
-        for(String text: result) {
-            System.out.println(text);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i));
+            if (i != result.size() -1) System.out.println();
         }
     }
 }
