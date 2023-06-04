f0c6683cb575365a19f9e29ecf29dcd28dc71634
Yong su Kwon <82052272+DevFrog92@users.noreply.github.com>
2023-06-04 21:34:01 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1052 ms, Memory: 141056 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -8,17 +8,54 @@ public class Main {
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         ArrayList<Integer> list = new ArrayList<>();
         int n = Integer.parseInt(reader.readLine());
+        int[] arr = new int[n];
+        int[] tmp = new int[n];
         for (int i = 0; i < n; i++) {
-            list.add(Integer.parseInt(reader.readLine()));
+            int num = Integer.parseInt(reader.readLine());
+            list.add(num);
+            arr[i] = num;
         }
-
-        list.sort(Comparator.naturalOrder());
-
-        for(int num: list) {
+        mergeSort(arr, tmp, 0, arr.length -1);
+        for(int num: arr) {
             writer.write(num + "\n");
         }
 
         writer.flush();
         writer.close();
+    }
+
+    public static void mergeSort(int[] arr, int[] tmp, int left, int right) {
+        if(left >= right){
+            return;
+        }
+
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, tmp, left, mid);
+        mergeSort(arr, tmp, mid+1, right);
+        merge(arr, tmp, left, right, mid);
+    }
+
+    public static void merge(int[] arr, int[] tmp, int left, int right, int mid) {
+        int p = left;
+        int q = mid +1;
+        int idx = left;
+
+        while(p <= mid || q <= right) {
+            if(p <= mid && q <= right) {
+                if(arr[p] <= arr[q]) {
+                    tmp[idx++] = arr[p++];
+                }else {
+                    tmp[idx++] = arr[q++];
+                }
+            }else if(p <= mid && q > right) {
+                tmp[idx++] = arr[p++];
+            }else {
+                tmp[idx++] = arr[q++];
+            }
+        }
+
+        for (int i = left; i <= right; i++) {
+            arr[i] = tmp[i];
+        }
     }
 }
