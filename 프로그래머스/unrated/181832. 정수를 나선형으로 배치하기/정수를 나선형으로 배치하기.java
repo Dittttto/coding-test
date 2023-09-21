cc64705c9e308b47aca2823d4ccb3e54477a8e2e
Ditto <82052272+DevFrog92@users.noreply.github.com>
2023-09-21 16:41:47 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.07 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -6,35 +6,35 @@ class Solution {
         int[] dy = new int[]{1,0,-1,0};
         int[] dx = new int[]{0,1,0,-1};
         
-        int cx = 0;
-        int cy = -1;
-        int cd = 0;
-        int cv = 0;
+        int curX = 0;
+        int curY = -1;
+        int curDirection = 0;
+        int curValue = 0;
         
-        while(cv < n*n) {
-            int nx = cx + dx[cd];
-            int ny = cy + dy[cd];
+        while(curValue < n*n) {
+            int nx = curX + dx[curDirection];
+            int ny = curY + dy[curDirection];
             
             if(nx < 0 || nx >= n) {
-                cd = (cd + 1) % 4;
+                curDirection = (curDirection + 1) % 4;
                 continue;
             }
             
             if(ny < 0 || ny >= n) {
-                cd = (cd + 1) % 4;
+                curDirection = (curDirection + 1) % 4;
                 continue;
             }
             
             if(map[nx][ny] != 0) {
-                cd = (cd + 1) % 4;
+                curDirection = (curDirection + 1) % 4;
                 continue;
             }
             
             
-            cx = nx;
-            cy = ny;
-            map[cx][cy] = cv + 1;
-            cv += 1;
+            curX = nx;
+            curY = ny;
+            map[curX][curY] = curValue + 1;
+            curValue += 1;
         }
         return map;
     }
