2f45ac94c9093b6ae8d34bd9d611e1e98172282d
Ditto <82052272+DevFrog92@users.noreply.github.com>
2023-10-16 22:37:23 +0900

    [Silver II] Title: DNA 비밀번호, Time: 532 ms, Memory: 85652 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,69 +1,61 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer srt = new StringTokenizer(reader.readLine());
-        int S = Integer.parseInt(srt.nextToken());
-        int p = Integer.parseInt(srt.nextToken());
-        String[] arr = reader.readLine().split("");
-        String[] less = reader.readLine().split(" ");
-        HashMap<String, Integer> need = new HashMap<>(){{
-            put("A", Integer.parseInt(less[0]));
-            put("C", Integer.parseInt(less[1]));
-            put("G", Integer.parseInt(less[2]));
-            put("T", Integer.parseInt(less[3]));
-        }};
-        HashMap<String, Integer> hm = new HashMap<>();
-
-        int left = 0;
-        int right = p - 1;
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] nums = reader.readLine().split(" ");
+        int n = Integer.parseInt(nums[0]);
+        int m = Integer.parseInt(nums[1]);
+        String[] dna = reader.readLine().split("");
+        int[] rule = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] init = new int[4];
         int cnt = 0;
-        for(int i = 0; i< p; i++) {
-            String cur = arr[i];
-            hm.put(cur, hm.getOrDefault(cur, 0) + 1);
+        int left =0;
+        int right = left + m - 1;
+
+        for (int i = 0; i < m; i++) {
+            int idx = getIdx(dna[i]);
+            init[idx] += 1;
         }
 
-        if(check(hm, need)) {
-            cnt++;
+        cnt += check(init, rule);
+
+        while(right < n) {
+            init[getIdx(dna[left])] -= 1;
+            left += 1;
+            right += 1;
+            if (right >= n) break;
+            init[getIdx(dna[right])] += 1;
+            cnt += check(init, rule);
         }
 
 
-        while(right < arr.length) {
-            hm.put(arr[left], hm.getOrDefault(arr[left], 0) - 1);
-            left++;
-            right++;
-
-            if(right >= arr.length) break;
-
-            hm.put(arr[right], hm.getOrDefault(arr[right], 0) + 1);
-
-            if(check(hm, need)) {
-                cnt++;
-            }
-        }
-
-        System.out.println(cnt);
+        writer.write(cnt + "");
+        writer.flush();
+        writer.close();
     }
 
-     public static boolean check(HashMap<String, Integer> hm, HashMap<String, Integer> need) {
-        for (Map.Entry<String, Integer> entry : need.entrySet()) {
-            String key = entry.getKey();
-            int value = entry.getValue();
-            if(value > 0) {
-                if(!hm.containsKey(key)) {
-                    return false;
-                }else {
-                    if(hm.get(key) < value){
-                        return false;
-                    }
-                }
-            }
+    private static int getIdx(String word) {
+        switch (word) {
+            case "A":
+                return 0;
+            case "C":
+                return 1;
+            case "G":
+                return 2;
+            default:
+                return 3;
+        }
+    }
+
+    private static int check(int[] init, int[] rule) {
+        int cnt = 0;
+        for (int i = 0; i < 4; i++) {
+            if (init[i] >= rule[i]) cnt += 1;
         }
 
-        return true;
+        return  cnt == 4 ? 1 : 0;
     }
 }
