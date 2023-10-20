ad9b7d861017daca6d2e8822b53a1877ebf46c54
Ditto <82052272+DevFrog92@users.noreply.github.com>
2023-10-20 15:18:09 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 45608 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,27 +1,29 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(reader.readLine());
+        Deque<Integer> queue = new LinkedList<>();
 
-        Deque<Integer> que = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
-            que.offerLast(i);
+            queue.offerLast(i);
         }
 
-         while(que.size() > 1) {
-             que.pollFirst();
-             if(!que.isEmpty()){
-                 int second = que.pollFirst();
-                 que.offerLast(second);
-             }
-         }
+        while (!queue.isEmpty() && queue.size() > 1) {
+            queue.pollFirst();
+            queue.offerLast(queue.pollFirst());
+        }
 
-         System.out.println(que.pollFirst());
+        writer.write(queue.peekLast() + "");
+        writer.flush();
+        writer.close();
     }
 }
