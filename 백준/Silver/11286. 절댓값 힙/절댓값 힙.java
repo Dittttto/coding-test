338459be08a5d938d45ebb143a27b20eaec772ad
Ditto <82052272+DevFrog92@users.noreply.github.com>
2023-10-20 15:35:12 +0900

    [Silver I] Title: 절댓값 힙, Time: 624 ms, Memory: 27412 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -7,29 +7,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-        PriorityQueue<Integer>  queue = new PriorityQueue<>(((o1, o2) -> {
-            int a = Math.abs(o1);
-            int b = Math.abs(o2);
+        PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2) -> {
+            int abs1 = Math.abs(o1);
+            int abs2 = Math.abs(o2);
 
-            if(a == b) {
-                return o1 < o2 ? -1 : 1;
+            if (abs1 == abs2) {
+                return o1 > o2 ? 1 : -1;
+            } else {
+                return abs1 - abs2;
             }
+        });
 
-            return a - b;
-        }));
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             int num = Integer.parseInt(reader.readLine());
-            if(num == 0) {
-                if(queue.isEmpty()) {
-                    System.out.println(0);
-                }else {
-                    System.out.println(queue.poll());
+            if (num == 0) {
+                if (q.isEmpty()) {
+                    System.out.println("0");
+                } else {
+                    System.out.println(q.poll());
                 }
-            }else {
-                queue.add(num);
+            } else {
+                q.offer(num);
             }
-
         }
     }
 }
