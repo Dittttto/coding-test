4051dcb3b29a025d82cab60a8e592a123f5f09c1
Ditto <82052272+DevFrog92@users.noreply.github.com>
2023-10-21 11:09:54 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 636 ms, Memory: 55644 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,28 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] input = reader.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
-
+        StringTokenizer srt = new StringTokenizer(reader.readLine());
+        int n = Integer.parseInt(srt.nextToken());
+        int m = Integer.parseInt(srt.nextToken());
         int[] arr = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] sumArr = new int[n];
-        sumArr[0] = arr[0];
-
+        int[] s = new int[n];
+        s[0] = arr[0];
         for (int i = 1; i < n; i++) {
-            sumArr[i] = sumArr[i-1] + arr[i];
+            s[i] = s[i - 1] + arr[i];
         }
 
-        for (int i = 0; i <m; i++) {
-            String[] toFrom = reader.readLine().split(" ");
-            int from = Integer.parseInt(toFrom[0]) - 2;
-            int to = Integer.parseInt(toFrom[1]) - 1;
-
-            writer.write(sumArr[to] - (from < 0 ? 0 : sumArr[from])+"\n");
+        for (int i = 0; i < m; i++) {
+            srt = new StringTokenizer(reader.readLine());
+            int l = Integer.parseInt(srt.nextToken())-1;
+            int r = Integer.parseInt(srt.nextToken())-1;
+            int result = l > 0 ? s[r] - s[l-1] : s[r];
+            writer.write(result + "\n");
         }
 
         writer.flush();
