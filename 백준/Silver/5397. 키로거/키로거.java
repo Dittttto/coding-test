05296f6820626231793efd507113d384738dcfea
Ditto <82052272+DevFrog92@users.noreply.github.com>
2023-12-07 23:00:47 +0900

    [Silver II] Title: 키로거, Time: 1008 ms, Memory: 175484 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -3,43 +3,42 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
-
+        static Deque<Character> left;
+        static Deque<Character> right;
         public static void main(String[] args) throws IOException {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
                 int n = Integer.parseInt(reader.readLine());
                 List<Character> pw;
                 for (int i = 0; i < n; i++) {
-                         pw = new LinkedList<>();
-                        int idx = 0;
+                         left = new ArrayDeque<>();
+                         right = new ArrayDeque<>();
 
                         for (Character s : reader.readLine().toCharArray()) {
                                 if(s == '-'){
-                                        if (idx > 0) {
-                                                idx -= 1;
-                                                pw.remove(idx);
-                                        }
+                                        if (left.isEmpty()) continue;
+                                        left.pollLast();
                                 }else if(s == '<') {
-                                        if (idx > 0) {
-                                                idx -= 1;
-                                        }
+                                        if (left.isEmpty()) continue;
+                                        right.offerFirst(left.pollLast());
                                 }else if(s == '>') {
-                                        if (idx + 1 <= pw.size()) {
-                                                idx += 1;
-                                        }
+                                        if (right.isEmpty()) continue;
+                                        left.offerLast(right.pollFirst());
                                 }else {
-                                        pw.add(idx, s);
-                                        idx += 1;
+                                        left.offerLast(s);
                                 }
                         }
 
 
                         StringBuilder sb = new StringBuilder();
-                        for (Character s : pw) {
+                        left.addAll(right);
+                        for (Character s : left) {
                                 sb.append(s);
                         }
                         writer.write(sb + "\n");
