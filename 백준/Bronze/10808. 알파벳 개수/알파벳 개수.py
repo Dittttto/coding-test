0a0bd04e5df2e88e6cd8ccc634bca2bfca9d67e5
Ditto <82052272+DevFrog92@users.noreply.github.com>
2024-01-03 09:48:06 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -3,12 +3,9 @@ import sys
 input = sys.stdin.readline
 
 words = list(input().strip())
-alphabet = list(map(chr, range(ord('a'), ord('z')+1)))
-count = {k:0 for k in alphabet}
-
+alph = [0] * 26
 for i in words:
-    count[i] += 1
+    alph[(ord(i) - ord('a'))] += 1
 
-
-for v in count.values():
-    print(v, end=" ")
+for i in alph:
+    print(i, end=" ")
