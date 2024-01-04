572f701076a7809ae78d09154f12f587f4665e3b
Ditto <82052272+DevFrog92@users.noreply.github.com>
2024-01-04 09:39:06 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -5,9 +5,3 @@ from (
     where date_format(APNT_YMD, '%Y-%m-%d') = '2022-04-13' and APNT_CNCL_YN = 'N' and MCDP_CD = 'CS'
 ) a inner join DOCTOR d on a.MDDR_ID = d.DR_ID inner join PATIENT p on a.PT_NO = p.PT_NO
 order by APNT_YMD;
-
-# select *
-# from APPOINTMENT a
-# inner join patient p on p.pt_no = a.pt_no
-# inner join doctor d on d.dr_id = a.mddr_id
-# where date_format(APNT_YMD, '%Y-%m-%d') = '2022-04-13';
