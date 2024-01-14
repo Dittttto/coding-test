dc3f2eeedd9227e9f0639b892964f54fcb6b047c
Ditto <82052272+DevFrog92@users.noreply.github.com>
2024-01-14 15:42:21 +0900

    [level Lv.3] Title: 단어 변환, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/Lv.3/43163. 단어 변환/단어 변환.py
@@ -3,26 +3,26 @@ from collections import deque
 memo = []
 def bfs(b, t, d, words):
     q = deque()
-    q.append(b)
+    q.append((b, 0))
     memo.append(b)
-    count = 1
 
     while q:
         temp = len(q)
         for _ in range(temp):
-            cur = q.pop()
+            cur, count = q.pop()
             if cur == t:
-                return count-1
-            
+                return count
             for j in range(len(cur)):
                 idx = j+1
                 for v in d[idx]:
                     newW = cur[:j] + v + cur[j+1:]
+                    print(cur,":",newW)
                     if newW not in words: continue
                     if newW in memo: continue
                     memo.append(newW)
-                    q.append(newW)
-        count += 1
+                    q.append((newW, count+1))
+                print(q)
+        print(count)
 
 def solution(begin, target, words):
     d = {}
@@ -31,7 +31,7 @@ def solution(begin, target, words):
         d[i+1] = set()
         for j in words:
             d[i+1].add(j[i:i+1])
-
+    print(d)
     if target not in words:
         return 0
     
