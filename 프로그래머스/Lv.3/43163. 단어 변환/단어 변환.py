c8839e28f4705444fe12b5ba9976a48a63ba873b
Ditto <82052272+DevFrog92@users.noreply.github.com>
2024-01-14 15:46:19 +0900

    [level Lv.3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/Lv.3/43163. 단어 변환/단어 변환.py
@@ -1,38 +1,37 @@
 from collections import deque
 
-memo = []
-def bfs(b, t, d, words):
+def bfs(begin, target, alphabetDict, words, memo):
     q = deque()
-    q.append((b, 0))
-    memo.append(b)
+    q.append((begin, 0))
+    memo.append(begin)
 
     while q:
         temp = len(q)
+        
         for _ in range(temp):
-            cur, count = q.pop()
-            if cur == t:
+            cur, count = q.popleft()
+            
+            if cur == target:
                 return count
+            
             for j in range(len(cur)):
-                idx = j+1
-                for v in d[idx]:
+                for v in alphabetDict[j+1]:
                     newW = cur[:j] + v + cur[j+1:]
-                    print(cur,":",newW)
                     if newW not in words: continue
                     if newW in memo: continue
+                    
                     memo.append(newW)
                     q.append((newW, count+1))
-                print(q)
-        print(count)
-
+                    
 def solution(begin, target, words):
-    d = {}
-    
-    for i in range(len(words[0])):
-        d[i+1] = set()
-        for j in words:
-            d[i+1].add(j[i:i+1])
-    print(d)
     if target not in words:
         return 0
     
-    return bfs(begin, target, d, words)
+    alphabetDict = {}
+    for i in range(len(words[0])):
+        alphabetDict[i+1] = set()
+        for j in words:
+            alphabetDict[i+1].add(j[i:i+1])
+
+    
+    return bfs(begin, target, alphabetDict, words, [])
