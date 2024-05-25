b447e6b4c66e4aa1458443e3a5276e281becb219
Ditto <82052272+Dittttto@users.noreply.github.com>
2024-05-25 19:40:19 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 9.97 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,16 +1,12 @@
 def solution(s):
-    x = s
     cnt = 0
     num_of_zero = 0
-    zero_cnt = 0
     while s != '1':
         length = len(s)
-        has_zero = False
         for i in s:
             if i == '0':
                 num_of_zero += 1
                 length -= 1
-                has_zero = True
             
         s = "{0:b}".format(length)
         cnt += 1
