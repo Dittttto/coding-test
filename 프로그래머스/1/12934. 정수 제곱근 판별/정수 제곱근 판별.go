a496cb6a8c353c6e80a8d2ef519000257784ead0
Ditto <82052272+Dittttto@users.noreply.github.com>
2024-05-29 13:10:33 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.00 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.go
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.go
@@ -2,12 +2,13 @@ func solution(n int64) int64 {
     if n == 1 {
         return 4
     }
+    
     if n < 3 {
         return -1
     }
     
-    var i int64 = 2
-    for ; i*i <= n; i++ {
+    var i int64
+    for i = 2 ; i*i <= n; i++ {
         if i*i == n {
             return (i+1) * (i+1)
         }
