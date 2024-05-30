968ee8519863c1f9e604a465f969d9aa961dbbcb
Ditto <82052272+Dittttto@users.noreply.github.com>
2024-05-30 17:25:37 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.03 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.go
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.go
@@ -1,10 +1,7 @@
 func solution(x int, n int) []int64 {
     var arr []int64
-    
-    c := 0
-    for i := x ; c < n ; i+= x{
-        arr = append(arr, int64(i))
-        c += 1
+    for i := 1 ; i <= n ; i++ {
+        arr = append(arr, int64(i * x))
     } 
     
     return arr
