7c55cc41cee22f95b1f233500e74a1d6a2532133
Ditto <82052272+Dittttto@users.noreply.github.com>
2024-06-01 00:42:46 +0900

    [level 1] Title: 음양 더하기, Time: 0.00 ms, Memory: 3.73 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.go
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.go
@@ -1,11 +1,3 @@
 func solution(absolutes []int, signs []bool) int {
-    answer := 0
-    for i := 0; i < len(absolutes); i++ {
-        if signs[i] {
-            answer += absolutes[i]
-        }else {
-            answer -= absolutes[i]
-        }
-    }
-    return answer
+    return 123456789
 }
