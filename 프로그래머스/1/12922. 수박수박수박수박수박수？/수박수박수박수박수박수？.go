b4a48c1d6cbfb34234cfa5e0b4b96c9a6adefde5
Ditto <82052272+Dittttto@users.noreply.github.com>
2024-06-06 00:50:26 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 4.29 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.go
@@ -0,0 +1,12 @@
+func solution(n int) string {
+    answer := ""
+    for i := 0 ; i < n ; i++ {
+        if i % 2 == 0 {
+            answer += "수"
+        } else {
+            answer += "박"
+        }
+        
+    }
+    return answer
+}
