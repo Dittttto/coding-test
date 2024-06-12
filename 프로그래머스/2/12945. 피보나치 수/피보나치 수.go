7db58cb71d82c1bf467534cb89eba0fefcc34c8d
Ditto <82052272+Dittttto@users.noreply.github.com>
2024-06-12 14:12:42 +0900

    [level 2] Title: 피보나치 수, Time: 3.70 ms, Memory: 7.86 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.go
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.go
@@ -1,3 +1,5 @@
+const Mod = 1234567
+
 func solution(n int) int {
     return Fino(n)
 }
@@ -6,8 +8,8 @@ func Fino(n int) int {
     memo := []int{0,1}
     
     for i := 2; i < n + 1; i++ {
-        memo = append(memo, (memo[i-1] % 1234567) + (memo[i-2] % 1234567))
+        memo = append(memo, (memo[i-1] % Mod) + (memo[i-2] % Mod))
     }
     
-    return memo[n] % 1234567
+    return memo[n] % Mod
 }
