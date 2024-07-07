dfdc41aad0082cf33422436caa3618b15dc88afa
Ditto <82052272+Dittttto@users.noreply.github.com>
2024-07-07 21:38:09 +0900

    [level 1] Title: 예산, Time: 0.49 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -2,21 +2,17 @@ import java.util.*;
 import java.util.Collections;
 class Solution {
     public int solution(int[] d, int budget) {
-        List<Integer> arr = new ArrayList<>();
-        for(int i : d){
-            arr.add(i);
-        }
-        Collections.sort(arr);
-        
+        Arrays.sort(d);
         int answer = 0;
-        int curr = 0;
-        
-        for(Integer a: arr) {
-            if(curr + a <= budget) {
-                curr += a;
-                answer += 1;
-            }
+
+        for(int i: d) {
+            budget -= i;
+            
+            if(budget < 0) break;
+            
+            answer += 1;
         }
+        
         return answer;
     }
 }
